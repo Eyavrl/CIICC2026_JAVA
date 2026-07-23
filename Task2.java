87e3740403a6a509abd6594486ccac199a37d65a
Eyavrl <bheamuana08@gmail.com>
2026-07-23 16:33:52 +0800

    Create Task2.java

---
+++ Task2.java
@@ -0,0 +1,16 @@
+public class Task2 {
+    public static void main(String[] args) {
+        char h = 'H';
+        byte three = 3;
+        short eleven = 11;
+        byte zero = 0;
+        char space = ' ';
+        String world = "world";
+        float twoPointZero = 2.0f;
+        boolean isTrue = true;
+
+        String output = "" + h + three + eleven + zero + space + world + space + twoPointZero + space + isTrue;
+
+        System.out.println(output);
+    }
+}
